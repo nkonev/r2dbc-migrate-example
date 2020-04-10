08de0a6563b03be3af85bad454de00d8dfd1ff20
Nikita Konev <nikit.cpp@yandex.ru>
2020-04-10 21:29:39 +0300

    initial library without spring

--- src/main/java/name/nkonev/r2dbc/migrate/example/CustomerApplication.java
+++ src/main/java/name/nkonev/r2dbc/migrate/example/CustomerApplication.java
@@ -1,30 +1,86 @@
 package name.nkonev.r2dbc.migrate.example;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.r2dbc.spi.Connection;
+import io.r2dbc.spi.ConnectionFactories;
 import io.r2dbc.spi.ConnectionFactory;
 import io.r2dbc.spi.ConnectionFactoryOptions;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.r2dbc.ConnectionFactoryOptionsBuilderCustomizer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import name.nkonev.r2dbc.migrate.core.R2dbcMigrate;
+import name.nkonev.r2dbc.migrate.core.R2dbcMigrateProperties;
+import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.netty.DisposableServer;
+import reactor.netty.http.server.HttpServer;
+import java.io.ByteArrayOutputStream;
+import static io.r2dbc.spi.ConnectionFactoryOptions.*;
 
-import java.time.Duration;
-
-@SpringBootApplication
-@RestController
 public class CustomerApplication {
 
-    @Autowired
-    private ConnectionFactory connectionFactory;
+    static final ObjectMapper mapper = new ObjectMapper();
 
-    public static void main(String[] args) {
-        SpringApplication.run(CustomerApplication.class, args);
+    private static ByteBuf toByteBuf(Object any) {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try {
+            mapper.writeValue(out, any);
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return ByteBufAllocator.DEFAULT
+                .buffer()
+                .writeBytes(out.toByteArray());
     }
 
+    public static void main(String[] args) {
+        R2dbcMigrateProperties properties = new R2dbcMigrateProperties();
+        properties.setResourcesPath("classpath:/db/migration/*.sql");
+        R2dbcMigrate.migrate(() -> makeConnectionMono(), properties).block();
+
+        ConnectionFactory connectionFactory = getConnectionFactory();
+        DisposableServer server =
+                HttpServer.create()
+                        .port(8383)
+                        .route(routes ->
+                                routes.get("/customer",
+                                        (request, response) -> {
+                                            Flux<ByteBuf> byteBufFlux = Mono.from(connectionFactory.create())
+                                                    .flatMapMany(connection -> Flux.from(connection.createStatement("SELECT * FROM customer ORDER BY id").execute()).concatMap(o -> o.map((row, rowMetadata) -> {
+                                                        Integer id = row.get("id", Integer.class);
+                                                        String firstName = row.get("first_name", String.class);
+                                                        String lastName = row.get("last_name", String.class);
+                                                        return new Customer(id, firstName, lastName);
+                                                    })).doFinally(signalType -> connection.close()))
+                                                    .map(CustomerApplication::toByteBuf);
+                                            return response.send(byteBufFlux);
+                                        }))
+                        .bindNow();
+
+        server.onDispose()
+                .block();
+    }
+
+    private static Mono<Connection> makeConnectionMono() {
+        ConnectionFactory connectionFactory = getConnectionFactory();
+        Publisher<? extends Connection> connectionPublisher = connectionFactory.create();
+        return Mono.from(connectionPublisher);
+    }
+
+    private static ConnectionFactory getConnectionFactory() {
+        return ConnectionFactories.get(ConnectionFactoryOptions.builder()
+                    .option(DRIVER, "postgresql")
+                    .option(HOST, "127.0.0.1")
+                    .option(PORT, 4433)
+                    .option(USER, "app_user")
+                    .option(PASSWORD, "password")
+                    .option(DATABASE, "customer_db")
+                    .build());
+    }
+
+
+    /*
     @GetMapping("/customer")
     public Flux<Customer> getCustomer() {
         return Mono.from(connectionFactory.create())
@@ -41,6 +97,6 @@ public class CustomerApplication {
         // PostgresqlConnectionFactoryProvider
         // ConnectionFactoryOptions
         return builder -> builder.option(ConnectionFactoryOptions.CONNECT_TIMEOUT, Duration.ofSeconds(10));
-    }
+    }*/
 
 }
