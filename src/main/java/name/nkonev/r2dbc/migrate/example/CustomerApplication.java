62298cca450e484b5280790148cba68170d6b560
Nikita Konev <nikit.cpp@yandex.ru>
2020-05-28 00:36:37 +0300

    use spring-data-r2dbc

--- src/main/java/name/nkonev/r2dbc/migrate/example/CustomerApplication.java
+++ src/main/java/name/nkonev/r2dbc/migrate/example/CustomerApplication.java
@@ -1,26 +1,23 @@
 package name.nkonev.r2dbc.migrate.example;
 
-import io.r2dbc.spi.Connection;
-import io.r2dbc.spi.ConnectionFactory;
 import io.r2dbc.spi.ConnectionFactoryOptions;
+import java.time.Duration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.r2dbc.ConnectionFactoryOptionsBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.r2dbc.core.DatabaseClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-import java.time.Duration;
 
 @SpringBootApplication
 @RestController
 public class CustomerApplication {
 
     @Autowired
-    private ConnectionFactory connectionFactory;
+    private DatabaseClient databaseClient;
 
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
@@ -28,12 +25,12 @@ public class CustomerApplication {
 
     @GetMapping("/customer")
     public Flux<Customer> getCustomer() {
-      return Flux.usingWhen(connectionFactory.create(), connection -> Flux.from(connection.createStatement("SELECT * FROM customer ORDER BY id").execute()).concatMap(o -> o.map((row, rowMetadata) -> {
-          Integer id = row.get("id", Integer.class);
-          String firstName = row.get("first_name", String.class);
-          String lastName = row.get("last_name", String.class);
-          return new Customer(id, firstName, lastName);
-        })), Connection::close);
+        return databaseClient.execute("SELECT * FROM customer ORDER BY id").map((row, rowMetadata) -> {
+            Integer id = row.get("id", Integer.class);
+            String firstName = row.get("first_name", String.class);
+            String lastName = row.get("last_name", String.class);
+            return new Customer(id, firstName, lastName);
+        }).all();
     }
 
     @Bean
