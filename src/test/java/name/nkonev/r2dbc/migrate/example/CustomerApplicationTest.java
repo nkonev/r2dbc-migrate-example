e380722b1f6049f564f8237fe5157e505114534e
Nikita Konev <nikit.cpp@yandex.ru>
2025-12-12 23:46:15 +0300

    update to 4.0.1

--- src/test/java/name/nkonev/r2dbc/migrate/example/CustomerApplicationTest.java
+++ src/test/java/name/nkonev/r2dbc/migrate/example/CustomerApplicationTest.java
@@ -4,17 +4,29 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
-import java.util.List;
-import static org.hamcrest.CoreMatchers.equalTo;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+@AutoConfigureWebTestClient
 public class CustomerApplicationTest {
 
     @Autowired
     private WebTestClient webClient;
+
+    @ServiceConnection
+    @Container
+    static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:14.1-alpine3.14");
 
     @Test
     @DisplayName("Should return list")
@@ -24,11 +36,12 @@ public class CustomerApplicationTest {
                 .exchange()
                 .expectStatus().isOk()
                 .expectBodyList(Customer.class)
-                .value(List::size, equalTo(8))
-                .value(customers -> customers.get(0).getFirstName(), equalTo("Test Customer Name 1"))
-                .value(customers -> customers.get(1).getFirstName(), equalTo("Funny User Name 2"))
-                .value(customers -> customers.get(2).getLastName(), equalTo("Unicode escape as workaround"))
-                .value(customers -> customers.get(7).getFirstName(), equalTo("Only Test"))
+                .value(customers -> {
+                    assertThat(customers.get(0).getFirstName()).isEqualTo("Test Customer Name 1");
+                    assertThat(customers.get(1).getFirstName()).isEqualTo("Funny User Name 2");
+                    assertThat(customers.get(2).getLastName()).isEqualTo("Unicode escape as workaround");
+                    assertThat(customers.get(7).getFirstName()).isEqualTo("Only Test");
+                })
         ;
     }
 
@@ -40,7 +53,9 @@ public class CustomerApplicationTest {
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(Customer.class)
-                .value(Customer::getFirstName, equalTo("Test Customer Name 1"))
+                .value(customer -> {
+                    assertThat(customer.getFirstName()).isEqualTo("Test Customer Name 1");
+                })
         ;
     }
 }
